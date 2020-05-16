37c3f373466ca023194395de3c2d068ae8eccc5d
Ameen Khan <ameenkhan@Ameens-MacBook-Pro.local>
2020-05-15 22:35:53 -0400

    creating a quick blog for practice

---
+++ my-blog/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+import './App.css';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesList from './pages/ArticlesList';
+import ArticlePage from './pages/ArticlePage';
+import NavBar from './NavBar';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div id="page-body">
+          <Route path="/" component={HomePage} exact/>
+          <Route path="/about" component={AboutPage} />
+          <Route path="/articles-list" component={ArticlesList} exact/>
+          <Route path="/article/:name" component={ArticlePage} exact/>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
